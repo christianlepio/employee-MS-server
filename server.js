343a515b67b92b4de00011c4bf8ef9e6effb2ab5
christianlepio <rylepio79@gmail.com>
2024-01-23 02:02:42 +0800

    create logger middleware & views

--- server.js
+++ server.js
@@ -6,3 +6,12 @@ const path = require('path')
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
+const PORT = process.env.PORT || 3500
+
+app.use(express.json())
+
+app.use(cookieParser())
+
+app.use('/', express.static(path.join(__dirname, 'public')))
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
