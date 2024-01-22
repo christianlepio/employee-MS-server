90b1ec600570ff5ce5fec9f5fb566c41299f1202
christianlepio <rylepio79@gmail.com>
2024-01-23 03:38:46 +0800

    create config for allowed origins & CORS options

---
+++ config/allowedOrigins.js
@@ -0,0 +1,7 @@
+// to request data from other domain (ie: google.com)
+// create a list that is allowed to access the backend (REST API)
+const allowedOrigins = [
+    'http://localhost:5173' //React JS local url'
+]
+
+module.exports = allowedOrigins
