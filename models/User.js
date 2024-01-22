c7fe32bb32db9df7f2c150d2b0ee1e2eafcf015d
christianlepio <rylepio79@gmail.com>
2024-01-23 03:28:29 +0800

    create user model

---
+++ models/User.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+
+// define user schema
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    }, 
+    password: {
+        type: String,
+        required: true
+    }, 
+    firstName: {
+        type: String,
+        required: true
+    }, 
+    lastName: {
+        type: String,
+        required: true
+    }, 
+    bdate: {
+        type: Date,
+        required: true
+    }, 
+    roles: {
+        type: [String],
+        default: ['Employee']
+    }, 
+    active: {
+        type: Boolean,
+        default: true
+    }
+})
+
+// create a model
+// User is a singular and will find its equivalent plural form in the collections from mongoDB
+module.exports = mongoose.model('User', userSchema)
